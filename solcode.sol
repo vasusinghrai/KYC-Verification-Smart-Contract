1da9f0e799d02dbdf55a1d62296020cdbac3279e
vasusinghrai <vasusinghrai1@gmail.com>
2025-04-25 20:44:09 +0530

    Update solcode.sol

--- solcode.sol
+++ solcode.sol
@@ -4,12 +4,6 @@ pragma solidity ^0.8.0;
 contract KYCVerification {
     address public admin;
 
-    struct User {// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract KYCVerification {
-    address public admin;
-
     struct User {
         string name;
         string documentHash;
@@ -17,6 +11,7 @@ contract KYCVerification {
     }
 
     mapping(address => User) public users;
+    address[] public userAddresses;
 
     modifier onlyAdmin() {
         require(msg.sender == admin, "Only admin can perform this action");
@@ -28,7 +23,9 @@ contract KYCVerification {
     }
 
     function addUser(address userAddress, string memory name, string memory documentHash) public onlyAdmin {
+        require(bytes(users[userAddress].name).length == 0, "User already exists");
         users[userAddress] = User(name, documentHash, false);
+        userAddresses.push(userAddress);
     }
 
     function verifyUser(address userAddress) public onlyAdmin {
@@ -36,39 +33,36 @@ contract KYCVerification {
         users[userAddress].isVerified = true;
     }
 
-    function getUser(address userAddress) public view returns (string memory, string memory, bool) {
-        User memory u = users[userAddress];
-        return (u.name, u.documentHash, u.isVerified);
-    }
-}
-
-        string name;
-        string documentHash;
-        bool isVerified;
-    }
-
-    mapping(address => User) public users;
-
-    modifier onlyAdmin() {
-        require(msg.sender == admin, "Only admin can perform this action");
-        _;
-    }
-
-    constructor() {
-        admin = msg.sender;
-    }
-
-    function addUser(address userAddress, string memory name, string memory documentHash) public onlyAdmin {
-        users[userAddress] = User(name, documentHash, false);
-    }
-
-    function verifyUser(address userAddress) public onlyAdmin {
+    function removeUser(address userAddress) public onlyAdmin {
         require(bytes(users[userAddress].name).length != 0, "User not found");
-        users[userAddress].isVerified = true;
+        delete users[userAddress];
+
+        // Remove address from the array
+        for (uint i = 0; i < userAddresses.length; i++) {
+            if (userAddresses[i] == userAddress) {
+                userAddresses[i] = userAddresses[userAddresses.length - 1];
+                userAddresses.pop();
+                break;
+            }
+        }
+    }
+
+    function updateUser(address userAddress, string memory newName, string memory newDocumentHash) public onlyAdmin {
+        require(bytes(users[userAddress].name).length != 0, "User not found");
+        users[userAddress].name = newName;
+        users[userAddress].documentHash = newDocumentHash;
+    }
+
+    function isUserVerified(address userAddress) public view returns (bool) {
+        return users[userAddress].isVerified;
     }
 
     function getUser(address userAddress) public view returns (string memory, string memory, bool) {
         User memory u = users[userAddress];
         return (u.name, u.documentHash, u.isVerified);
+    }
+
+    function getAllUsers() public view returns (address[] memory) {
+        return userAddresses;
     }
 }
